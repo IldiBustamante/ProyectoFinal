f130d4870ffc93c21348c67941d5aa8978625e31
ildibustamante <ild.sweet@hotmail.com>
2019-07-30 02:05:48 -0300

    cambios proyecto final imagen arreglada detalle

--- Solucion/ProyectoTechnoloshe/Models/TechnolosheProyecto.Designer.cs
+++ Solucion/ProyectoTechnoloshe/Models/TechnolosheProyecto.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Administrator\Desktop\Codigo Ildara\ProyectoTechnoloshe\Solucion\ProyectoTechnoloshe\Models\TechnolosheProyecto.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Administrator\Desktop\Codigo Ildara\ProyectoTechnoloshe Ildara\Solucion\ProyectoTechnoloshe\Models\TechnolosheProyecto.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
